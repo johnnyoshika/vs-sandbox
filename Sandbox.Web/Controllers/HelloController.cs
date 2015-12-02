8ebded7aa356c5ba32a715240acfafd8a6c47cb3
Johnny Oshika <johnnyoshika@gmail.com>
2015-12-02 10:37:26 -0800

    Added HelloController. #2

---
+++ Sandbox.Web/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sandbox.Web.Controllers
+{
+    public class HelloController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Bye!");
+        }
+    }
+}
