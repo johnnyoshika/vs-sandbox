391db6e3ecf113b17ed96c5b2cf55d226d9ec1a6
Johnny Oshika <johnnyoshika@gmail.com>
2015-12-07 16:31:47 -0800

    Added Sandbox.Web2

---
+++ Sandbox.Web2/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sandbox.Web2
+{
+    public class Book
+    {
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+
+        public string Author { get; }
+        public string Title { get; }
+    }
+}
