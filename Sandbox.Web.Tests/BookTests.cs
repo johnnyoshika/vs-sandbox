b8064f6d33cf43be14e2cf7121fcbec0b6968d59
Johnny Oshika <johnnyoshika@gmail.com>
2015-12-07 17:01:19 -0800

    Added another book author test.

--- Sandbox.Web.Tests/BookTests.cs
+++ Sandbox.Web.Tests/BookTests.cs
@@ -20,5 +20,12 @@ namespace Sandbox.Web.Tests
             var book = new Book("", "Mike");
             Assert.AreEqual("Mike", book.Author);
         }
+
+        [TestMethod]
+        public void Book_Constructor_Assigns_Author2()
+        {
+            var book = new Book("", "Bob");
+            Assert.AreEqual("Bob", book.Author);
+        }
     }
 }
